64bfc97bbc1fe78af570f5eba25ae1c3eeb44fd0
Denis Mordvinov <8133967+Sublio@users.noreply.github.com>
2026-06-01 13:13:26 +0600

    Add: redistribute-characters-to-make-all-strings-equal - submission-0

---
+++ Data Structures & Algorithms/redistribute-characters-to-make-all-strings-equal/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def makeEqual(self, words: List[str]) -> bool:
+        if len(words) == 1:
+            return True
+        
+        char_count = {}
+        for word in words:
+            for char in word:
+                char_count[char] = char_count.get(char, 0) + 1
+        
+        n = len(words)
+        
+        for count in char_count.values():
+            if count % n != 0:
+                return False
+        
+        return True
+        
